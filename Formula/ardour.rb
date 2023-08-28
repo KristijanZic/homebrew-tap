ca568bf62504aceca7a1637c0db6b209bd0ccc90
KristijanZic <kristijan.zic@gmail.com>
2023-08-28 14:18:47 +0200

    first commit

---
+++ Formula/ardour.rb
@@ -0,0 +1,58 @@
+class Ardour < Formula
+  desc "A digital audio workstation"
+  homepage "https://ardour.org/"
+  url "https://github.com/Ardour/ardour/archive/refs/tags/7.5.tar.gz"
+  sha256 "56787c0f17092e715cad5a8086ba994f819004cf1568b558226dcd3568cd7f87"
+  version "7.5"
+  head "https://github.com/Ardour/ardour.git"
+  license "GNU GPL-2.0"
+
+  depends_on "aubio"
+  depends_on "boost"
+  depends_on "fftw"
+  depends_on "glib"
+  depends_on "glibmm"
+  depends_on "jack"
+  depends_on "libarchive"
+  depends_on "liblo"
+  depends_on "libsndfile"
+  depends_on "libusb"
+  depends_on "lrdf"
+  depends_on "lv2"
+  depends_on "pangomm"
+  depends_on "pkg-config" => :build
+  depends_on "rubberband"
+  depends_on "serd"
+  depends_on "sord"
+  depends_on "sratom"
+  depends_on "gtkmm"
+  depends_on "lilv"
+  depends_on "taglib"
+  depends_on "vamp-plugin-sdk"
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    # system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "./waf", "configure", "--prefix=#{prefix}", "--with-backends=jack"
+    system "./waf", "install"
+
+    cd "./tools/osx_packaging" do
+      system "./osx_build", "--help"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test ardour`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
